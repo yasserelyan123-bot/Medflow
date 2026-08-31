5d153549245f9eff22064488ec1ebb7e877e8ed6
harafy-app <yasserelyan123@gmail.com>
2026-08-31 22:11:08 +0300

    Delete client.ts

--- client.ts
+++
@@ -1,7 +0,0 @@
-const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || 'http://localhost:8000/api/v1'
-export async function api<T>(path:string, options:RequestInit={}):Promise<T>{
- const r=await fetch(`${API_BASE_URL}${path}`,{headers:{'Content-Type':'application/json',...(options.headers||{})},...options})
- if(!r.ok) throw new Error(await r.text())
- return r.json()
-}
-export default API_BASE_URL
