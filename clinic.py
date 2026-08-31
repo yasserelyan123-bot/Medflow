57585a52dc626008ccbbdb70d306010eab4f085b
harafy-app <yasserelyan123@gmail.com>
2026-08-31 22:11:19 +0300

    Delete clinic.py

--- clinic.py
+++
@@ -1,7 +0,0 @@
-from pydantic import BaseModel
-from typing import Optional
-class ClinicCreate(BaseModel):
-    name: str
-    phone: Optional[str] = None
-    email: Optional[str] = None
-    address: Optional[str] = None
