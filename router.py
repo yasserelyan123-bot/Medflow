b961fdec22c62858db956ea0b1b07407ea951fdf
harafy-app <yasserelyan123@gmail.com>
2026-08-31 22:14:36 +0300

    Delete router.py

--- router.py
+++
@@ -1,7 +0,0 @@
-from fastapi import APIRouter
-from app.api.v1.endpoints import patients, appointments, visits, dashboard
-api_router = APIRouter()
-api_router.include_router(patients.router, prefix="/patients", tags=["Patients"])
-api_router.include_router(appointments.router, prefix="/appointments", tags=["Appointments"])
-api_router.include_router(visits.router, prefix="/visits", tags=["Visits"])
-api_router.include_router(dashboard.router, prefix="/dashboard", tags=["Dashboard"])
